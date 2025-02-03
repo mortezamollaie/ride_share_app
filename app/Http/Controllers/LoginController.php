d8168f3ac51918b4d7aac50461f9c2253c60228c
mortezamollaie <mm.gov.1381@gmail.com>
2025-02-03 11:28:25 +0330

    implement user verify and loged in user info

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,15 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Medianasms\Errors\Error;
+use Medianasms\Errors\HttpException;
 
 class LoginController extends Controller
 {
+    /**
+     * @throws HttpException
+     * @throws Error
+     */
     public function submit(Request $request)
     {
         // validate the phone number
         $request->validate([
             'phone' => 'required|numeric|min:11'
         ]);
+
 
         // find or create a user model
         $user = User::firstOrCreate([
@@ -27,12 +34,45 @@ class LoginController extends Controller
 
         // send a user a one time use code
         $LoginCode = rand(111111,999999);
+
         $user->login_code = $LoginCode;
         $user->save();
 
         // return back a response
         return response()->json([
-            'message' => 'Text message notification sent.'
+            'message' => 'Text message notification sent.',
+            'login_code' => $LoginCode
         ]);
+    }
+
+    public function verify(Request $request)
+    {
+        // validate incoming request
+        $request->validate([
+            'phone' => 'required|numeric|min:11',
+            'login_code' => 'required|numeric|between:111111,999999'
+        ]);
+
+        // find the user
+        $user = User::where('phone', $request->phone)
+            ->where('login_code', $request->login_code)
+            ->first();
+
+        // is the code provided the same one saved?
+
+        // if so, return back an auth token
+
+        if($user){
+            $user->update([
+                'login_code' => null
+            ]);
+            return response()->json([
+               'token' => $user->createToken($request->login_code)->plainTextToken
+            ]);
+        }
+        // if not, return back a message
+        return response()->json([
+            'message' => 'Invalid verification code.'
+        ], 401);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserInfo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [LoginController::class, 'submit']);
+Route::post('/login/verify', [LoginController::class, 'verify']);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/user', UserInfo::class);
+});
