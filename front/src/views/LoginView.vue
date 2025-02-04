e31452e0abf80753af80ff49ad27c868c563178e
mortezamollaie <mm.gov.1381@gmail.com>
2025-02-04 12:51:59 +0330

    fix loginview component

---
+++ front/src/views/LoginView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="pt-16">
+        <h1 class="text-3xl font-semibold mb-4">Enter your phone number</h1>
+        <form action="#">
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+                <div class="bg-white px-4 py05 sm:p-6">
+                    <div>
+                        <input type="text" name="phone" id="phone" placeholder="09123456789" class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 focus:ring-opacity-50">
+                    </div>
+                </div>
+                <div class="bg-gray-100 px-4 py-3 text-right sm:px-6">
+                    <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-black/80 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2">
+                        Continue
+                    </button>
+                </div>
+            </div>
+
+        </form>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
+
+
