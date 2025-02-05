f7a0ffb3ff7d4c8e1ac9c0086ea16b86fa821763
mortezamollaie <mm.gov.1381@gmail.com>
2025-02-05 11:54:58 +0330

    send request from front

--- front/src/views/LoginView.vue
+++ front/src/views/LoginView.vue
@@ -1,15 +1,16 @@
 <template>
     <div class="pt-16">
         <h1 class="text-3xl font-semibold mb-4">Enter your phone number</h1>
-        <form action="#">
+        <form action="#" @submit.prevent="handleLogin">
             <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
                 <div class="bg-white px-4 py05 sm:p-6">
                     <div>
-                        <input type="text" v-maska="'#### ### ####'" name="phone" id="phone" placeholder="09123456789" class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 focus:ring-opacity-50">
+                        <input type="text" v-maska="'#### ### ####'" v-model="credentials.phone" name="phone" id="phone" placeholder="09123456789" class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 focus:ring-opacity-50">
                     </div>
                 </div>
+
                 <div class="bg-gray-100 px-4 py-3 text-right sm:px-6">
-                    <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-black/80 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2">
+                    <button type="submit" @submit.prevent="handleLogin" class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-black/80 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2">
                         Continue
                     </button>
                 </div>
@@ -21,10 +22,32 @@
 
 <script setup>
 import { vMaska } from "maska/vue"
+import { reactive } from 'vue'
+import axios from 'axios'
+
+const credentials = reactive({
+    phone: null
+})
+
+const handleLogin = () => {
+    credentials.phone = credentials.phone.replace(/\s/g, '')
+    axios.post('http://127.0.0.1:8000/api/login', credentials)
+    .then(response => {
+        console.log(response.data)
+    })
+
+    .catch(error => {
+        console.log(error)
+    })
+}
+
+
+
 </script>
 
 <style>
 
 </style>
+
 
 
