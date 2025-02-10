6a31f1d3c47c8ce4e2d5ad5aca7d3ba3b3bea054
mortezamollaie <mm.gov.1381@gmail.com>
2025-02-10 12:38:05 +0330

    adding channels to front

--- front/src/views/StandbyView.vue
+++ front/src/views/StandbyView.vue
@@ -10,7 +10,23 @@
 </template>
 
 <script setup>
-
 import Loader from '@/components/Loader.vue';
+import { onMounted } from 'vue';
+import Echo from 'laravel-echo';
+
+onMounted(() => {
+    let echo = new Echo({
+        broadcaster: 'pusher',
+        key: 'mykey',
+        cluster: 'mt1',
+        wsHost: window.location.hostname,
+        wsPort: 6001,
+        forceTLS: false,
+        disableStats: true,
+        enableTransports: ['ws', 'wss'],
+    })
+
+    echo.channel()
+})
 
 </script>
